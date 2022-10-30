e4ec319ee1223180299e83168ca999867a0931c0
Chayanika Khatua <chayanika.khatua@gmail.com>
2022-10-30 17:55:13 +0400

    day 2

--- 2021/src/2.py
+++ 2021/src/2.py
@@ -1,13 +1,34 @@
-def part1():
+def readInstructions():
     instructionStrings = open('../inputs/2.txt').read().split('\n')
-    instructions = [ tuple(str.split(' ')) for str in instructionStrings ]
+    instructionsStringParts = [ tuple(str.split(' ')) for str in instructionStrings ]
+    instructions = [(a, int(b)) for (a,b) in instructionsStringParts]
+    return instructions
+
+def part1():
+    instructions = readInstructions()
     # the order doesn't seem to matter in part 1
     # solution using list comprehensions seems slow, are list comprehensions not performant?
     # applying list comprehensions, yay
-    forwardValues = [ int(b) for a,b in instructions if a == 'forward']
-    upValues = [ int(b) for a,b in instructions if a == 'up']
-    downValues = [ int(b) for a,b in instructions if a == 'down']
+    forwardValues = [ b for a,b in instructions if a == 'forward']
+    upValues = [ b for a,b in instructions if a == 'up']
+    downValues = [ b for a,b in instructions if a == 'down']
 
     return sum(forwardValues) * (sum(downValues) - sum(upValues))
 
-print(part1())
+def part2():
+    aim = 0
+    hor_pos = 0
+    depth = 0
+    instructions = readInstructions()
+    for a,b in instructions:
+        if a == 'up':
+            aim = aim - b
+        elif a == 'down':
+            aim = aim + b
+        else:
+            hor_pos = hor_pos + b
+            depth = aim * b + depth
+    return hor_pos * depth
+    
+print('part 1', part1())
+print('part 2', part2())
